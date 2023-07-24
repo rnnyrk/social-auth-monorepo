57c05dbc2250f272ae2d1deded6aadb146c1adbb
Ronny Rook <info@ronnyrook.nl>
2023-07-24 16:26:33 +0200

    Run Expo from monorepo with pnpm

---
+++ apps/native/app.config.ts
@@ -0,0 +1,32 @@
+import path from 'path';
+import type { ConfigContext, ExpoConfig } from '@expo/config';
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: 'expo-native',
+  slug: 'native',
+  version: '1.0.0',
+  orientation: 'portrait',
+  icon: path.resolve(__dirname, 'assets/icon.png'),
+  userInterfaceStyle: 'light',
+  splash: {
+    image: path.resolve(__dirname, 'assets/splash.png'),
+    resizeMode: 'contain',
+    backgroundColor: '#ffffff'
+  },
+  updates: {
+    fallbackToCacheTimeout: 0
+  },
+  assetBundlePatterns: ['**/*'],
+  ios: {
+    supportsTablet: false,
+    bundleIdentifier: 'com.expo.native',
+    buildNumber: '1',
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: path.resolve(__dirname, 'assets/adaptive-icon.png'),
+      backgroundColor: '#FFFFFF'
+    }
+  },
+});
--- apps/native/metro.config.js
+++ apps/native/metro.config.js
@@ -1,21 +1,50 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require("expo/metro-config");
-const path = require("path");
+// const { getDefaultConfig } = require("expo/metro-config");
+// const path = require("path");
 
-// Find the workspace root, this can be replaced with `find-yarn-workspace-root`
-const workspaceRoot = path.resolve(__dirname, "../..");
-const projectRoot = __dirname;
+// // Find the workspace root, this can be replaced with `find-yarn-workspace-root`
+// const workspaceRoot = path.resolve(__dirname, "../..");
+// const projectRoot = __dirname;
 
-const config = getDefaultConfig(projectRoot);
+// const config = getDefaultConfig(projectRoot);
 
-// 1. Watch all files within the monorepo
-config.watchFolders = [workspaceRoot];
-// 2. Let Metro know where to resolve packages, and in what order
-config.resolver.nodeModulesPaths = [
-  path.resolve(projectRoot, "node_modules"),
-  path.resolve(workspaceRoot, "node_modules"),
-];
-// 3. Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
-config.resolver.disableHierarchicalLookup = true;
+// // 1. Watch all files within the monorepo
+// config.watchFolders = [workspaceRoot];
+// // 2. Let Metro know where to resolve packages, and in what order
+// config.resolver.nodeModulesPaths = [
+//   path.resolve(projectRoot, "node_modules"),
+//   path.resolve(workspaceRoot, "node_modules"),
+// ];
+// // 3. Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
+// config.resolver.disableHierarchicalLookup = true;
 
-module.exports = config;
+// module.exports = config;
+
+const { getDefaultConfig } = require('@expo/metro-config');
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+const { makeMetroConfig } = require('@rnx-kit/metro-config');
+const MetroSymlinksResolver = require('@rnx-kit/metro-resolver-symlinks');
+
+const symlinkResolver = MetroSymlinksResolver();
+
+const requestResolver = (context, moduleName, platform, realName) => {
+  // patch for unpatched dependencies
+  // e.g. react-native-version-check-expo
+  if (moduleName === '@unimodules/core') {
+    const expoModules = 'expo-modules-core';
+    return symlinkResolver(context, expoModules, platform, expoModules);
+  }
+
+  return symlinkResolver(context, moduleName, platform, realName);
+};
+
+module.exports = {
+  ...defaultConfig,
+  ...makeMetroConfig({
+    projectRoot: __dirname,
+    resolver: {
+      resolveRequest: requestResolver,
+    },
+  }),
+};
